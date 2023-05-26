a0b5a3c4563e1c9705f303df98d3b6e92fe641f3
nasmog12345 <126059506+nasmog12345@users.noreply.github.com>
2023-05-26 20:14:34 +0200

    closes Junit Testfunktion #11

--- src/test/java/ch/zhaw/carewe/NeedyControllerTest.java
+++ src/test/java/ch/zhaw/carewe/NeedyControllerTest.java
@@ -1,6 +1,10 @@
 package ch.zhaw.carewe;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,38 +21,43 @@ import static org.mockito.ArgumentMatchers.any;
 
 import java.util.Arrays;
 
+@ExtendWith(MockitoExtension.class)
 class NeedyControllerTest {
-    private NeedyRepository needyRepository = Mockito.mock(NeedyRepository.class);
-    private NeedyController needyController = new NeedyController();
+    @InjectMocks
+    private NeedyController needyController;
+
+    @Mock
+    private NeedyRepository needyRepository;
 
     @Test
-void createNeedyTest() {
-    // Erstellen Sie ein Mock NeedyCreateDTO
-    NeedyCreateDTO mockNDTO = new NeedyCreateDTO();
-    mockNDTO.setName("Test Name");
-    mockNDTO.setEmail("test@example.com");
-    mockNDTO.setAddress("Test Address");
-    mockNDTO.setNumber("1234567890");
-    mockNDTO.setNeeds(Arrays.asList("Need1", "Need2"));
-    mockNDTO.setNotes("Test Notes");
-    mockNDTO.setNeedyState(NeedyState.UNALLOCATED);
-    
-    // Erstellen Sie ein Mock Needy
-    Needy mockNeedy = new Needy(mockNDTO.getName(), mockNDTO.getEmail(), mockNDTO.getAddress(), mockNDTO.getNumber(),
-        mockNDTO.getNeeds(), mockNDTO.getNotes(), mockNDTO.getNeedyState());
-    
-    // Mocken Sie das Verhalten der save-Methode
-    Mockito.when(needyRepository.save(any(Needy.class))).thenReturn(mockNeedy);
-    
-    // Testen Sie die Methode
-    ResponseEntity<Needy> response = needyController.createNeedy(mockNDTO);
-    
-    // Überprüfen Sie das Ergebnis
-    assertEquals(mockNeedy, response.getBody());
-    assertEquals(HttpStatus.CREATED, response.getStatusCode());
-    
-    // Überprüfen Sie, ob die save-Methode aufgerufen wurde
-    verify(needyRepository).save(any(Needy.class));
-}
+    void createNeedyTest() {
+        // Erstellen Sie ein Mock NeedyCreateDTO
+        NeedyCreateDTO mockNDTO = new NeedyCreateDTO();
+        mockNDTO.setName("Test Name");
+        mockNDTO.setEmail("test@example.com");
+        mockNDTO.setAddress("Test Address");
+        mockNDTO.setNumber("1234567890");
+        mockNDTO.setNeeds(Arrays.asList("Need1", "Need2"));
+        mockNDTO.setNotes("Test Notes");
+        mockNDTO.setNeedyState(NeedyState.UNALLOCATED);
+
+        // Erstellen Sie ein Mock Needy
+        Needy mockNeedy = new Needy(mockNDTO.getName(), mockNDTO.getEmail(), mockNDTO.getAddress(),
+                mockNDTO.getNumber(),
+                mockNDTO.getNeeds(), mockNDTO.getNotes(), mockNDTO.getNeedyState());
+
+        // Mocken Sie das Verhalten der save-Methode
+        Mockito.when(needyRepository.save(any(Needy.class))).thenReturn(mockNeedy);
+
+        // Testen Sie die Methode
+        ResponseEntity<Needy> response = needyController.createNeedy(mockNDTO);
+
+        // Überprüfen Sie das Ergebnis
+        assertEquals(mockNeedy, response.getBody());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        // Überprüfen Sie, ob die save-Methode aufgerufen wurde
+        verify(needyRepository).save(any(Needy.class));
+    }
 
 }
